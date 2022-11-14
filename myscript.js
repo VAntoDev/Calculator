aa502f6cc970680e28710bda8a0ee362470ed570
VAnto <vntantonio@gmail.com>
2022-11-14 13:03:23 +0100

    Add js-numbers btn

--- myscript.js
+++ myscript.js
@@ -14,7 +14,7 @@ const divide = function(firstNum, secondNum) {
     return firstNum / secondNum;
 };
 
-function operate(operator, firstNum, secondNum){
+function operate(firstNum, operator, secondNum){
 
     switch(operator) {                                
         case '+':                                 
@@ -54,4 +54,63 @@ const multiplyMoreNum = function(array) {
   return totalMultiply;
 };
 
-sum()
+
+let currentOperation = 0;
+
+const storeOperation = function(number) {
+    currentOperation = number + currentOperation;
+    results.textContent = currentOperation;
+}
+
+const results = document.querySelector("#results")
+
+const numberOneBtn = document.querySelector("#one");
+numberOneBtn.addEventListener("click", function (){
+    storeOperation(1);
+});
+
+const numberTwoBtn = document.querySelector("#two");
+numberTwoBtn.addEventListener("click", function (){
+    storeOperation(2);
+});
+
+const numberThreeBtn = document.querySelector("#three");
+numberThreeBtn.addEventListener("click", function (){
+    storeOperation(3);
+});
+
+const numberFourBtn = document.querySelector("#four");
+numberFourBtn.addEventListener("click", function (){
+    storeOperation(4);
+});
+
+const numberFiveBtn = document.querySelector("#five");
+numberFiveBtn.addEventListener("click", function (){
+    storeOperation(5);
+});
+
+const numberSixBtn = document.querySelector("#six");
+numberSixBtn.addEventListener("click", function (){
+    storeOperation(6);
+});
+
+const numberSevenBtn = document.querySelector("#seven");
+numberSevenBtn.addEventListener("click", function (){
+    storeOperation(7);
+});
+
+const numberEightBtn = document.querySelector("#eight");
+numberEightBtn.addEventListener("click", function (){
+    storeOperation(8);
+});
+
+const numberNineBtn = document.querySelector("#nine");
+numberNineBtn.addEventListener("click", function (){
+    storeOperation(9);
+});
+
+const numberZeroBtn = document.querySelector("#zero");
+numberZeroBtn.addEventListener("click", function (){
+    storeOperation(0);
+});
+
